b390d729e2b924ea3d3903df568ca0993fca7464
Pepemanpro <91446514+Pepemanpro@users.noreply.github.com>
2023-11-22 21:17:42 -0500

    Add files via upload

---
+++ publicador1.py
@@ -0,0 +1,21 @@
+import time
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Conectado al broker")
+
+def on_publish(client, userdata, mid):
+    print("Mensaje publicado")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_publish = on_publish
+
+client.connect("localhost", 1883)
+
+# Bucle infinito para publicar mensajes
+while True:
+    message = "Hola soy el publicador 1"
+    client.publish("topic/example", message)
+    time.sleep(10)
+
---
+++ suscriptor1.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Conectado al broker")
+
+def on_message(client, userdata, message):
+    print("Mensaje recibido Soy S1: " + message.payload.decode())
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883)
+client.subscribe("topic/example")
+
+# Bucle infinito para recibir mensajes
+while True:
+    client.loop()
